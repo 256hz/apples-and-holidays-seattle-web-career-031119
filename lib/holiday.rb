b83b2bc9c0aead8608c09446c5959ba1a3132e6b
Abe D <iaryiou111@gmail.com>
2019-03-05 22:40:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, value|
     puts "#{season.to_s.capitalize}:"
     holiday_hash[season].each do |holiday, supplies_array|
-      if holiday.array.size > 1
+      if holiday.include?(" ")
         holiday_string = holiday.to_s.split(" ")
       else
         holiday_string = holiday.to_s
